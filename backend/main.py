45b1c0f80cba202b65d1c3f0cfbad389ce8eb521
Deep-Kumawat <deepdkmail@gmail.com>
2024-03-28 21:39:14 +0530

    listing data is successfully being returned for internshala todo: - account for user's skills - sort acc to skills

--- backend/main.py
+++ backend/main.py
@@ -1,38 +1,17 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from flask import jsonify, Flask, request
+from scrappers import scrapeInternshala, scrapeNaukriDotCom
+import json
 
-# Define the HTTP request handler class
-class MyHTTPRequestHandler(BaseHTTPRequestHandler):
-    # Handler for the GET requests
-    def do_GET(self):
-        if(self.path == "/getdata"):
-            # Set response status code
-            self.send_response(200)
-            # Set headers
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            # Write response content
-            self.wfile.write(b"<html><head><title>Sample HTTP Server</title></head>")
-            self.wfile.write(b"<body><h1>Hello, World!</h1></body></html>")
-        else:
-            # Set response status code
-            self.send_response(200)
-            # Set headers
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            # Write response content
-            self.wfile.write(b"<html><head><title>Sample HTTP Server</title></head>")
-            self.wfile.write(b"<body><h1>Hello, World!</h1></body></html>")
+app = Flask(__name__)
 
-# Define the main function to start the server
-def main():
-    try:
-        server = HTTPServer(('localhost', 8080), MyHTTPRequestHandler)
-        print('HTTP server started on http://localhost:8080')
-        server.serve_forever()
-    except KeyboardInterrupt:
-        print('^C received, shutting down the server')
-        server.socket.close()
+@app.route("/")
+def home():
+    naukriDotComData = scrapeNaukriDotCom("web developer", 0, "vadodara")
+    internShalaData = scrapeInternshala("web developer", "vadodara")
+    # naukriDotComData = json.dumps(naukriDotComData.json)
+    internShalaData = json.dumps(internShalaData.json, indent=2)
+    print("done")
+    return internShalaData
 
-# Entry point of the script
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    app.run(debug=True)
--- backend/scrappers.py
+++ backend/scrappers.py
@@ -7,22 +7,19 @@ from selenium.webdriver.chrome.options import Options
 from selectolax.parser import HTMLParser
 from bs4 import BeautifulSoup
 import pandas as pd
-from time import sleep
+from flask import jsonify, Response
 
-
-def scrapeNaukriDotCom(title="", experience=0, location = ""):
+headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+    "AppId" : "109",
+    "SystemId" : "Naukri"
+}
+def scrapeNaukriDotCom(title: str, experience: int, location: str) -> Response:
     titleSEOKey = title.replace(" ", "-")
     title = title.replace(" ", "%20")
     
     testURL = f"https://www.naukri.com/jobapi/v3/search?noOfResults=20&urlType=search_by_key_loc&searchType=adv&location={location}&keyword={title}&pageNo=1&experience={experience}&k={title}&l={location}&experience={experience}&seoKey={titleSEOKey}-jobs-in-{location}&src=jobsearchDesk&latLong="
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-        "AppId" : "109",
-        "SystemId" : "Naukri"
-    }
     resp = requests.get(testURL, headers=headers)
-    with open("foo2.json", 'w') as f:
-        json.dump(resp.json(), f)
     respJson = resp.json()
 
     listings = {"title": [],
@@ -44,26 +41,15 @@ def scrapeNaukriDotCom(title="", experience=0, location = ""):
         listings["companyName"].append(jobDetailCompanyName)
         listings["skills"].append(jobDetailTagsAndSkills)
         listings["jobURL"].append(jobDetailJobURL)
-    df = pd.DataFrame(listings)
-    print(df)
+
+    listings = jsonify(listings)
+    return listings
 
     
-
-
-scrapeNaukriDotCom("web develoment", 0, "vadodara")
-
-
-
-
-
-
-def scrapeInternshala(profile, location):
+def scrapeInternshala(profile: str, location: str) -> Response:
     # for now we are only supporting profile and location
     # profile is a mendatory argument
     URL = "https://internshala.com/internships/"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
-    }
     profile = profile.strip().lower().replace(' ', '-') + "-internship/"
     location = location.strip().lower().replace(' ', '-')
     if(location != ''):
@@ -71,48 +57,72 @@ def scrapeInternshala(profile, location):
     else:
         URL = f"https://internshala.com/internships/work-from-home-{profile}-internships/part-time-true/"
     
-    listings = {"title": [],
+    listingsCSVStyle = {"title": [],
                 "companyName": [],
                 "skills": [],
+                "jobURL": []
                 }
     resp = httpx.get(URL, headers=headers)
     soup = BeautifulSoup(resp, 'html.parser')
     numberOfPages = int(soup.find(id="total_pages").text)
-    print(numberOfPages)
-    for i in range(1, numberOfPages+1):
+    # for i in range(1, numberOfPages+1):
+    for i in range(1, 2):
         URL = URL + f"page-{i}/"
         resp = httpx.get(URL, headers=headers)
         print(URL)
-
-
         soup = BeautifulSoup(resp, 'html.parser')
-        try:
-            internshipListingCards = soup.find_all(class_="container-fluid individual_internship visibilityTrackerItem")
-            for internshipListingCard in internshipListingCards:
-                #Getting title and company name
-                internshipListingCardTitleAndCompany = internshipListingCard.div.find(class_ = "individual_internship_header").find(class_="company")
-                internshipListingCardTitle = internshipListingCardTitleAndCompany.find(class_="heading_4_5 profile")
-                internshipListingCardCompanyName = internshipListingCardTitleAndCompany.find(class_="heading_6 company_name")
-                
-                listings["title"].append(internshipListingCardTitle.text.strip())
-                listings["companyName"].append(internshipListingCardCompanyName.text.strip())
-                
-                #Getting skills
-                internshipListingCardDetailsPageLink = "https://internshala.com" + internshipListingCard.find(class_="button_container_card").div.a['href']
-                detailsPage = httpx.get(internshipListingCardDetailsPageLink, headers=headers)
-                detailsPageHTML = BeautifulSoup(detailsPage, "html.parser")
+        internshipListingCards = soup.find_all(class_="container-fluid individual_internship visibilityTrackerItem")
+        for internshipListingCard in internshipListingCards:
+            #Getting title and company name
+            internshipListingCardTitleAndCompany = internshipListingCard.div.find(class_ = "individual_internship_header").find(class_="company")
+            internshipListingCardTitle = internshipListingCardTitleAndCompany.find(class_="heading_4_5 profile")
+            internshipListingCardCompanyName = internshipListingCardTitleAndCompany.find(class_="heading_6 company_name")
+            
+            listingsCSVStyle["title"].append(internshipListingCardTitle.text.strip())
+            listingsCSVStyle["companyName"].append(internshipListingCardCompanyName.text.strip())
+            
+            #Getting skills
+            internshipListingCardDetailsPageLink = "https://internshala.com" + internshipListingCard.find(class_="button_container_card").div.a['href']
+            detailsPage = httpx.get(internshipListingCardDetailsPageLink, headers=headers)
+            detailsPageHTML = BeautifulSoup(detailsPage, "html.parser")
+            # in case no skills are found
+            try:
                 skillsFromDetailsPage = detailsPageHTML.find(class_ = "round_tabs_container").children
                 skills = []
                 for skill in skillsFromDetailsPage:
                     if(skill.text != "\n"):
                         skills.append(skill.text)
-                listings["skills"].append(skills)
-        except AttributeError as err:
-            print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-            print(URL)
-            print(err)
-    df = pd.DataFrame(listings)
-    print(df)
+                listingsCSVStyle["skills"].append(skills)
+            except AttributeError as err:
+                print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+                print(URL)
+                print(err)
+                skillsFromDetailsPage = "No skills found"
+                listingsCSVStyle["skills"].append(["No skills listed"])
+            listingsCSVStyle["jobURL"].append(internshipListingCardDetailsPageLink)
 
-    
-scrapeInternshala("web development", "vadodara")
+    # df = pd.DataFrame(listings)
+    # df.to_csv("hello.csv")
+    print(len(listingsCSVStyle["title"]))
+    print(len(listingsCSVStyle["companyName"]))
+    print(len(listingsCSVStyle["skills"]))
+    print(len(listingsCSVStyle["jobURL"]))
+    # print(df)
+    # ret = {"test": "testresponse"}
+    # jsonify(ret)
+    # print("jhkhjhkj",ret)
+    # return "ret"
+
+    listingsJsonStyle = []
+
+    for i in range(0, len(listingsCSVStyle["title"])):
+        temp = {}
+        temp["companyName"] = listingsCSVStyle["companyName"][i]
+        temp["title"] = listingsCSVStyle["title"][i]
+        temp["skills"] = listingsCSVStyle["skills"][i]
+        temp["jobURL"] = listingsCSVStyle["jobURL"][i]
+        listingsJsonStyle.append(temp)
+
+
+    listingsJsonStyle = jsonify(listingsJsonStyle)
+    return listingsJsonStyle
