0ac4cdca6539849c602c7cacca8989948ba68294
deep-kumawat <deepdkmail@gmail.com>
2024-05-29 02:39:34 +0530

    created the home page. Added two clickable divs for feild-known and feild-unknown options

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <>
+      <Home />
+
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,39 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+function Home() {
+    return (
+        <div className='text-center h-screen flex items-center space-x-8 justify-center'>
+
+            <a href="/feildknown">
+                <Card className='p-8 overflow-hidden transition-all ease-in-out duration-300 hover:shadow-xl max-w-96 max-h-96 flex items-center'>
+                    <CardHeader>
+                        <CardTitle>Feild Known</CardTitle>
+                        <CardDescription>You know the field you are interested in and you want to know which skills are the most in-demand for that field</CardDescription>
+                    </CardHeader>
+                    {/* <CardContent>
+        <p>Card Content</p>
+    </CardContent> */}
+                    {/* <Button>Button</Button> */}
+                </Card>
+            </a>
+            <a href="/feildunknown">
+                <Card className='p-8 overflow-hidden transition-all ease-in-out duration-300 hover:shadow-xl max-w-96 max-h-96 flex items-center'>
+                    <CardHeader>
+                        <CardTitle>Feild Unknown</CardTitle>
+                        <CardDescription>You don't know your field that's why you want to research which field would be best for you</CardDescription>
+                    </CardHeader>
+                    {/* <CardContent>
+        <p>Card Content</p>
+    </CardContent> */}
+                    {/* <Button>Button</Button> */}
+                </Card>
+            </a>
+        </div>
+    )
+}
+
+export default Home
