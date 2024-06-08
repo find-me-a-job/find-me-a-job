5c7e45d6f81df00e634cbc71152199ea85280014
zaid <102306774+zaidkaladia@users.noreply.github.com>
2024-06-08 20:43:50 +0530

    created a new function scrapeInternshalaV2, scrapping internshala and returning sorted list of skills according to its occurance, average salary and applicant to job ratio.

--- backend/scrappers.py
+++ backend/scrappers.py
@@ -1,12 +1,14 @@
 import httpx
+from selectolax.parser import HTMLParser
 import requests
 import json
 from bs4 import BeautifulSoup
 import pandas as pd
 from flask import Response
-
+from dataclasses import dataclass, asdict
+import collections
 headers = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
     "AppId" : "109",
     "SystemId" : "Naukri"
 }
@@ -48,17 +50,15 @@ def scrapeNaukriDotCom(title: str, experience: int, location: str) -> list:
         temp["jobURL"] = "https://www.naukri.com" + listingsCSVStyle["jobURL"][i]
         listingsJsonStyle.append(temp)
 
-
     # listingsJsonStyle = json.dumps(listingsJsonStyle)
     return listingsJsonStyle
     # listingsCSVStyle = json.dumps(listingsCSVStyle)
     # return listingsCSVStyle
-
     
 def scrapeInternshala(profile: str, location: str) -> list:
     # for now we are only supporting profile and location
     # profile is a mendatory argument
-    URL = "https://www.internshala.com/internships/"
+    URL = "https://www.internshala.com/internship/"
     profile = profile.strip().lower().replace(' ', '-') + "-internship/"
     location = location.strip().lower().replace(' ', '-')
     if(location != ''):
@@ -110,8 +110,6 @@ def scrapeInternshala(profile: str, location: str) -> list:
                 listingsCSVStyle["skills"].append(["No skills listed"])
             listingsCSVStyle["jobURL"].append(internshipListingCardDetailsPageLink)
 
-    
-
     listingsJsonStyle = []
 
     for i in range(0, len(listingsCSVStyle["title"])):
@@ -122,13 +120,137 @@ def scrapeInternshala(profile: str, location: str) -> list:
         temp["jobURL"] = listingsCSVStyle["jobURL"][i]
         listingsJsonStyle.append(temp)
 
-
-    # listingsJsonStyle = json.dumps(listingsJsonStyle)
     return listingsJsonStyle
 
+def scrapeInternshalaV2(profile="", location="", experience=0):
+    def get_html(page):
+        if(experience == 0):
+            # Fresher
+            url = f"https://internshala.com/fresher-jobs/{profile}-jobs-in-{location}/page-{page}"
+        else:
+            url = f"https://internshala.com/fresher-jobs/{profile}-jobs-in-{location}/experience-{experience}/page-{page}"
+        resp = httpx.get(url, headers=headers)
+        return HTMLParser(resp.text)
+    webpage = get_html(1)
+    number_of_pages = int(webpage.css_first("span#total_pages").text())
+    salaryList = []
+    skills = []
+    skillsDict = {}
+    applicants = []
+    for page_number in range(1, number_of_pages):
+        print("page", page_number, "/", number_of_pages)
+        webpage = get_html(page_number);
+        salaries = webpage.css("span.desktop")
+        # return
+        listingCards = webpage.css_first(f"#internship_list_container_{page_number}")
+        if(listingCards is None):
+            print("listing container was not found in the html so early returning!")
+            str = webpage.html
+            with open("output.txt", 'w+') as f:
+                f.write(str)
+            return
+        else:
+            listingCards = listingCards.iter()
+        listingLinks = []
+        for listingCard in listingCards:
+            try:
+                listingLinks.append("https://internshala.com" + listingCard.attributes['data-href'] + '/')
+            except KeyError:
+                print(KeyError, "data-href attribute was not found in listing div")
+                continue
+        totalListingsVisitedForSkills = 0
+        skillsNotFound = 0
+        number_of_applicants = 0
+        number_of_listings_scrapped_for_number_of_applicants = 0
+        # Scrapping Skills #
+        for url in listingLinks:
+            listingPage = HTMLParser(httpx.get(url, headers=headers).text)
+            # skillsExtractedRaw = listingPage.css("span.round_tabs")
+            skillsRawHTML = listingPage.css("span.round_tabs")
+            totalListingsVisitedForSkills += 1
+            # Number of applicants
+            # number_of_applicants_on_current_listing
+            if(len(skillsRawHTML) == 0):
+                print("LENGTH OF SKILLS_RAW_HTML WAS 0 - No skills div found.")
+                skillsNotFound += 1
+                continue
+            skills_for_current_listing = list(map(lambda x: x.text().strip().lower(), skillsRawHTML))
+            skills.append(skills_for_current_listing)
+
+            # Applicants
+            try:
+                number_of_applicants_in_current_listing = listingPage.css_first(".applications_message").text()[:-11].strip()
+                if(number_of_applicants_in_current_listing == "1000+"):
+                    number_of_applicants_in_current_listing = 1000
+                number_of_applicants += int(number_of_applicants_in_current_listing)
+                number_of_listings_scrapped_for_number_of_applicants += 1
+            except Exception as e:
+                print("something went wrong while scrapping number of applicants")
+                print(e)
+                print("-->", number_of_applicants_in_current_listing)
+                print("---------------XXXXXXXXXXXXXXXXXXXXXXXXXX---------------")
+            # Skills new format
+            for skill in skills_for_current_listing:
+                if(skill in skillsDict):
+                    skillsDict[skill] += 1
+                else:
+                    skillsDict[skill] = 1
+        print("totalListingsVisitedForSkills :", totalListingsVisitedForSkills, " / ", "skillsNotFound :", skillsNotFound)
+        for i in salaries:
+            if(i.text().strip() == "Competitive salary"):
+                continue
+            salaryRange = i.text().strip()[2:]
+            endIndexForFirstNumber = len(salaryRange)
+            startIndexForSecondNumber = len(salaryRange)
+            secondNumber = 0
+            divideFactor = 2
+            try:
+                endIndexForFirstNumber = salaryRange.index("-")
+                startIndexForSecondNumber = endIndexForFirstNumber + 2
+                secondNumber = int(salaryRange[startIndexForSecondNumber:].strip().replace(",", ""))
+            except ValueError as e:
+                divideFactor = 1
+            firstNumber = int(salaryRange[0:endIndexForFirstNumber].strip().replace(",", ""))
+            averageSalary = (firstNumber + secondNumber) // divideFactor
+            salaryList.append(averageSalary)
+    sumOfSalaries = 0
+    for i in salaryList:
+        sumOfSalaries += i
+    if(len(salaryList) > 0):
+        averageSalary = sumOfSalaries/len(salaryList)
+    else:
+        print("why is salaryList's length 0????")
+        print(salaryList)
+        print("------------------------------------")
+    
+    print(skillsDict)
+
+    with open("skillsDict.json", "w+") as f:
+        f.write(json.dumps(skillsDict))
+
+    sorted_items = sorted(skillsDict.items(), key=lambda x: x[1], reverse=True)
+
+    sorted_array = [item[0] for item in sorted_items]
+
+    skillsDict = sorted(skillsDict, reverse=False)
+    # Returing data
+    applicants_ratio_to_jobs_ratio = number_of_applicants / number_of_listings_scrapped_for_number_of_applicants
+    returnData = {}
+    returnData["skills"] = sorted_array
+    returnData["average-salary"] = averageSalary
+    returnData["applicants-to-jobs-ratio"] = applicants_ratio_to_jobs_ratio
+    with open("output.json", "w+") as f:
+        f.write(json.dumps(returnData))
+    
+    return returnData
+    
+
 def scrape(info: dict) -> json:
-    internshalaData = scrapeInternshala(info["title"], info["location"])
+    internshalaData = scrapeInternshalaV2(profile=info["title"], location=info["location"], experience=info["experience"])
     naukriDotComData = scrapeNaukriDotCom(info["title"], info["experience"], info["location"])
     response = internshalaData + naukriDotComData
     response = json.dumps(response)
     return response
+
+if __name__ == "__main__":
+    scrapeInternshalaV2("web-development", "", 0)
