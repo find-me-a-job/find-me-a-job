eac3f6ebe60ea46234853ee93cace5a1d732f96d
deep-kumawat <deepdkmail@gmail.com>
2024-06-12 02:43:30 +0530

    - deleted scrapeInternshala() - renamed scrapeInternshalaV2 --> scrapeInternshala() - changed schema of json response of scrapeInternshala() and scrapeNaukriDotCom() to { 	"skills" : dictionary of skills with frequency of skills as values 	"average-salary": } applicants-to-jobs-ratio metric is left to add.

--- backend/scrappers.py
+++ backend/scrappers.py
@@ -1,4 +1,4 @@
- import httpx
+import httpx
 from selectolax.parser import HTMLParser
 import requests
 import json
@@ -12,116 +12,58 @@ headers = {
     "SystemId" : "Naukri"
 }
 
-def scrapeNaukriDotCom(title: str, experience: int, location: str) -> list:
+def scrapeNaukriDotCom(title: str, location: str, experience: int) -> list:
     titleSEOKey = title.replace(" ", "-")
     title = title.replace(" ", "%20")
     
     testURL = f"https://www.naukri.com/jobapi/v3/search?noOfResults=20&urlType=search_by_key_loc&searchType=adv&location={location}&keyword={title}&pageNo=1&experience={experience}&k={title}&l={location}&experience={experience}&seoKey={titleSEOKey}-jobs-in-{location}&src=jobsearchDesk&latLong="
-    resp = requests.get(testURL, headers=headers)
-    respJson = resp.json()
-    listingsCSVStyle = {
-                "title": [],
-                "companyName": [],
-                "skills": [],
-                "jobURL": []
+    httpxResponse = httpx.get(testURL, headers=headers)
+    # print(resp.text)
+    # return
+    jsonResponse = httpxResponse.json()
+    if(jsonResponse["noOfJobs"] == 0):
+        return {}
+    print(jsonResponse["jobDetails"])
+    returnData = {
+                "skills": {},
             }
-    jobDetails = respJson["jobDetails"] 
+    jobDetails = jsonResponse["jobDetails"]
+    with open("temp.json", "w+") as f:
+        f.write(json.dumps(jobDetails))
+    skills = []
+    sumOfSalaries = 0
+    numberOfSalariesCalculated = 0
     for jobDetail in jobDetails:
         try:
-            jobDetailTitle = jobDetail["title"]
-            jobDetailCompanyName = jobDetail["companyName"]
-            jobDetailTagsAndSkills = jobDetail["tagsAndSkills"]
-            jobDetailJobURL = jobDetail["jdURL"]
+            #skills
+            skills = jobDetail["tagsAndSkills"].lower().split(",")
+            for skill in skills:
+                if skill in returnData["skills"]:
+                    returnData["skills"][skill] += 1
+                else:
+                    returnData["skills"][skill] = 1
+            #average-salary
+            salary = jobDetail["placeholders"][1]["label"]
+            if salary != "Not disclosed":
+                print(salary)
+                salary = float(salary[salary.index("-") + 1 : -8].strip())
+
+                sumOfSalaries += salary
+                numberOfSalariesCalculated += 1
+            # applicants-to-jobs-ratio
+
         except KeyError as err:
             continue
-        listingsCSVStyle["title"].append(jobDetailTitle)
-        listingsCSVStyle["companyName"].append(jobDetailCompanyName)
-        listingsCSVStyle["skills"].append(jobDetailTagsAndSkills)
-        listingsCSVStyle["jobURL"].append(jobDetailJobURL)
-    
-    listingsJsonStyle = []
-
-    for i in range(0, len(listingsCSVStyle["title"])):
-        temp = {}
-        temp["companyName"] = listingsCSVStyle["companyName"][i]
-        temp["title"] = listingsCSVStyle["title"][i]
-        temp["skills"] = listingsCSVStyle["skills"][i]
-        temp["jobURL"] = "https://www.naukri.com" + listingsCSVStyle["jobURL"][i]
-        listingsJsonStyle.append(temp)
-
-    # listingsJsonStyle = json.dumps(listingsJsonStyle)
-    return listingsJsonStyle
-    # listingsCSVStyle = json.dumps(listingsCSVStyle)
-    # return listingsCSVStyle
-    
-def scrapeInternshala(profile: str, location: str) -> list:
-    # for now we are only supporting profile and location
-    # profile is a mendatory argument
-    URL = "https://www.internshala.com/internship/"
-    profile = profile.strip().lower().replace(' ', '-') + "-internship/"
-    location = location.strip().lower().replace(' ', '-')
-    if(location != ''):
-        URL = f"https://internshala.com/internships/work-from-home-{profile}-internships-in-{location}/part-time-true/"
+    if(numberOfSalariesCalculated == 0):
+        returnData["average-salary"] = 0
     else:
-        URL = f"https://internshala.com/internships/work-from-home-{profile}-internships/part-time-true/"
+        returnData["average-salary"] = sumOfSalaries / numberOfSalariesCalculated
+    # with open("skills.json", "w+") as f:
+    #     f.write(json.dumps(returnData))
     
-    listingsCSVStyle = {"title": [],
-                    "companyName": [],
-                    "skills": [],
-                    "jobURL": []
-                }
-    resp = httpx.get(URL, headers=headers)
-    soup = BeautifulSoup(resp, 'html.parser')
-    numberOfPages = int(soup.find(id="total_pages").text)
-    # for i in range(1, numberOfPages+1):
-    for i in range(1, 2):
-        URL = URL + f"page-{i}/"
-        resp = httpx.get(URL, headers=headers)
-        print(URL)
-        soup = BeautifulSoup(resp, 'html.parser')
-        internshipListingCards = soup.find_all(class_="container-fluid individual_internship visibilityTrackerItem")
-        for internshipListingCard in internshipListingCards:
-            #Getting title and company name
-            internshipListingCardTitleAndCompany = internshipListingCard.div.find(class_ = "individual_internship_header").find(class_="company")
-            internshipListingCardTitle = internshipListingCardTitleAndCompany.find(class_="heading_4_5 profile")
-            internshipListingCardCompanyName = internshipListingCardTitleAndCompany.find(class_="heading_6 company_name")
-            
-            listingsCSVStyle["title"].append(internshipListingCardTitle.text.strip())
-            listingsCSVStyle["companyName"].append(internshipListingCardCompanyName.text.strip())
-            
-            #Getting skills
-            internshipListingCardDetailsPageLink = "https://internshala.com" + internshipListingCard.find(class_="button_container_card").div.a['href']
-            detailsPage = httpx.get(internshipListingCardDetailsPageLink, headers=headers)
-            detailsPageHTML = BeautifulSoup(detailsPage, "html.parser")
-            # in case no skills are found
-            try:
-                skillsFromDetailsPage = detailsPageHTML.find(class_ = "round_tabs_container").children
-                skills = []
-                for skill in skillsFromDetailsPage:
-                    if(skill.text != "\n"):
-                        skills.append(skill.text)
-                listingsCSVStyle["skills"].append(skills)
-            except AttributeError as err:
-                print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-                print(URL)
-                print(err)
-                skillsFromDetailsPage = "No skills found"
-                listingsCSVStyle["skills"].append(["No skills listed"])
-            listingsCSVStyle["jobURL"].append(internshipListingCardDetailsPageLink)
+    return returnData
 
-    listingsJsonStyle = []
-
-    for i in range(0, len(listingsCSVStyle["title"])):
-        temp = {}
-        temp["companyName"] = listingsCSVStyle["companyName"][i]
-        temp["title"] = listingsCSVStyle["title"][i]
-        temp["skills"] = listingsCSVStyle["skills"][i]
-        temp["jobURL"] = listingsCSVStyle["jobURL"][i]
-        listingsJsonStyle.append(temp)
-
-    return listingsJsonStyle
-
-def scrapeInternshalaV2(profile="", location="", experience=0):
+def scrapeInternshala(profile="", location="", experience=0):
     def get_html(page):
         if(experience == 0):
             # Fresher
@@ -229,21 +171,23 @@ def scrapeInternshalaV2(profile="", location="", experience=0):
 
     sorted_items = sorted(skillsDict.items(), key=lambda x: x[1], reverse=True)
 
-    sorted_array = [item[0] for item in sorted_items]
+    skillsSortedByFrequency = [item[0] for item in sorted_items]
 
-    skillsDict = sorted(skillsDict, reverse=False)
+    # skillsDict = sorted(skillsDict, reverse=False)
     # Returing data
     applicants_ratio_to_jobs_ratio = number_of_applicants / number_of_listings_scrapped_for_number_of_applicants
     returnData = {}
-    returnData["skills"] = sorted_array
+    returnData["skills"] = skillsDict
     returnData["average-salary"] = averageSalary
     returnData["applicants-to-jobs-ratio"] = applicants_ratio_to_jobs_ratio
+    with open("temp.json", "w+") as f:
+        f.write(json.dumps(web_development))
     
     return returnData
     
 
 def scrape(info: dict) -> json:
-    internshalaData = scrapeInternshalaV2(profile=info["title"], location=info["location"], experience=info["experience"])
+    internshalaData = scrapeInternshala(profile=info["title"], location=info["location"], experience=info["experience"])
     # naukriDotComData = scrapeNaukriDotCom(info["title"], info["experience"], info["location"])
     # response = internshalaData + naukriDotComData
     # response = json.dumps(response)
@@ -251,14 +195,21 @@ def scrape(info: dict) -> json:
     return json.dumps(internshalaData)
 
 if __name__ == "__main__":
-    web_development = scrapeInternshalaV2("web-development", "", 0)
-    data_science = scrapeInternshalaV2("data_science", "", 0)
-    cyber_security = scrapeInternshalaV2("cyber-security", "", 0)
-    cloud_computing = scrapeInternshalaV2("cloud-computing", "", 0)
+    # web_development = scrapeInternshala("web-development", "", 0)
+    # data_science = scrapeInternshala("data_science", "", 0)
+    # cyber_security = scrapeInternshala("cyber-security", "", 0)
+    # cloud_computing = scrapeInternshala("cloud-computing", "", 0)
 
-    print(web_development)
-    # print(data_science)
-    # print(cyber_security)
-    print(cloud_computing)
+    # print(web_development)
+    # # print(data_science)
+    # # print(cyber_security)
+    # print(cloud_computing)
 
-    #hello from deep
+    data = {}
+    data["web_development"] = scrapeNaukriDotCom("web-development", "", 0)
+    data["data_science"] = scrapeNaukriDotCom("data_science", "", 0)
+    data["cyber_security"] = scrapeNaukriDotCom("cyber-security", "", 0)
+    data["cloud_computing"] = scrapeNaukriDotCom("cloud-computing", "", 0)
+
+    with open("output.json", "a+") as f:
+        f.write(json.dumps(data))
