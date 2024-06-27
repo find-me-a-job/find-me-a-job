d80a4b821541701dc2b146a5471017d905277cc7
deep-kumawat <deepdkmail@gmail.com>
2024-06-28 04:51:56 +0530

    added naurki endpoint for only naukri.com data

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,7 +1,7 @@
 #PORT = 5000
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from scrappers import scrapeKnownField
+from scrappers import scrapeKnownField, scrapeNaukriDotCom
 import json
 
 app = Flask(__name__)
@@ -15,10 +15,16 @@ def home():
 def test():
     print("tsetprint")
     return "hello from test"
+
 @app.route("/api/v1/known-field-data", methods=["POST"])
 def data():
     info = json.loads(request.data.decode())
     return jsonify(scrapeKnownField(info))
+
+@app.route("/api/v1/naukri", methods=["POST"])
+def naukriData():
+    info = json.loads(request.data.decode())
+    return jsonify(scrapeNaukriDotCom(title=info["title"], location=info["location"], experience=info["experience"]))
 
 if __name__ == "__main__":
     app.run(host= "0.0.0.0", port=5000, debug=True)
